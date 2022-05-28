1fb86093379520a885bcb07e81b5d61f78e07853
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-05-28 09:11:56 +0100

    Fix syntax errors in 'number' test

--- kata.spec.js
+++ kata.spec.js
@@ -45,14 +45,14 @@ describe("number", () => {
       number([
         [10, 0],
         [3, 5],
-        [5, 18],
+        [5, 8],
       ])
     ).toEqual(5)
-    expect(number([3, 0], [9, 1], [4, 10], [12, 2], [6, 1], [7, 10])).toEqual(
+    expect(number([[3, 0], [9, 1], [4, 10], [12, 2], [6, 1], [7, 10]])).toEqual(
       17
     )
-    expect(number([3, 0], [9, 1], [4, 8], [12, 2], [6, 1], [7, 8])).toEqual(21)
-    expect(number([0, 0])).toEqual(0)
+    expect(number([[3, 0], [9, 1], [4, 8], [12, 2], [6, 1], [7, 8]])).toEqual(21)
+    expect(number([[0, 0]])).toEqual(0)
   })
 })
 
