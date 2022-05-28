3c96590dfd6c7fb02e113d826eb0ce2a4c52e91c
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-05-28 07:58:43 +0100

    Write 'countDevelopers' tests

--- kata.spec.js
+++ kata.spec.js
@@ -1,7 +1,4 @@
-const { 
-  monkeyCount, 
-  maps,
-} = require("./kata_solutions.js")
+const { monkeyCount, maps } = require("./kata_solutions.js")
 
 describe("monkeyCount", () => {
   it("should return an array of numbers from 1 to n", () => {
@@ -35,8 +32,16 @@ describe("removeCharacters", () => {
 
 describe("number", () => {
   it("should return the total of adding the 0th and subtracting the 1st elements in each sub array", () => {
-    expect(number([[10, 0], [3, 5], [5, 18]])).toEqual(5)
-    expect(number([3, 0], [9, 1], [4, 10], [12, 2], [6, 1], [7, 10])).toEqual(17)
+    expect(
+      number([
+        [10, 0],
+        [3, 5],
+        [5, 18],
+      ])
+    ).toEqual(5)
+    expect(number([3, 0], [9, 1], [4, 10], [12, 2], [6, 1], [7, 10])).toEqual(
+      17
+    )
     expect(number([3, 0], [9, 1], [4, 8], [12, 2], [6, 1], [7, 8])).toEqual(21)
     expect(number([0, 0])).toEqual(0)
   })
@@ -44,10 +49,81 @@ describe("number", () => {
 
 describe("ballCollector", () => {
   it("Should return the total of all occurences of the value 58 in the array", () => {
-    expect(ballCollector(58, 68, 62, 69, 58)).toEqual({weight: 116})
-    expect(ballCollector(60, 66, 71, 68, 62)).toEqual({weight: 0})
-    expect(ballCollector(58)).toEqual({weight: 58})
-    expect(ballCollector(58, 58, 58)).toEqual({weight: 174})
+    expect(ballCollector(58, 68, 62, 69, 58)).toEqual({ weight: 116 })
+    expect(ballCollector(60, 66, 71, 68, 62)).toEqual({ weight: 0 })
+    expect(ballCollector(58)).toEqual({ weight: 58 })
+    expect(ballCollector(58, 58, 58)).toEqual({ weight: 174 })
   })
 })
 
+describe("countDevelopers", () => {
+  const list1 = [
+    {
+      firstName: "Noah",
+      lastName: "M.",
+      country: "Switzerland",
+      continent: "Europe",
+      age: 19,
+      language: "JavaScript",
+    },
+    {
+      firstName: "Maia",
+      lastName: "S.",
+      country: "Tahiti",
+      continent: "Oceania",
+      age: 28,
+      language: "JavaScript",
+    },
+    {
+      firstName: "Shufen",
+      lastName: "L.",
+      country: "Taiwan",
+      continent: "Asia",
+      age: 35,
+      language: "HTML",
+    },
+    {
+      firstName: "Sumayah",
+      lastName: "M.",
+      country: "Tajikistan",
+      continent: "Asia",
+      age: 30,
+      language: "CSS",
+    },
+  ]
+
+    it("should return the total count of JavaScript developers from Europe", () => {
+      try {
+        expect(countDevelopers(list1)).toEqual(1)
+      } catch (e) {
+        console.error("Noah is the only JavaScript developer from Europe")
+      }
+    })
+
+  const list2 = [
+    {
+      firstName: "Oliver",
+      lastName: "Q.",
+      country: "Australia",
+      continent: "Oceania",
+      age: 19,
+      language: "HTML",
+    },
+    {
+      firstName: "Lukas",
+      lastName: "R.",
+      country: "Austria",
+      continent: "Europe",
+      age: 89,
+      language: "HTML",
+    },
+  ]
+
+  it("should not find HTML developers from Europe", () => {
+    try {
+      expect(countDevelopers(list2)).toEqual(0)
+    } catch (e) {
+      console.error("Lukas is not a JavaScript developer from Europe")
+    }
+  })
+})
